2db12bcdec30d9314f9242346119076a5aa735c7
Kennedy <kennedysgarage@gmail.com>
2013-06-06 22:43:33 -0400

    adding some major base improvments

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Keystone
 
-Keystone is starting place in the construction of web apps that look like native apps.
+Keystone is starting place in the construction of web apps.
 
 ## Features
  * Launch Image
@@ -8,3 +8,6 @@ Keystone is starting place in the construction of web apps that look like native
  * Loading Screen
  * Link stay within the app
  * Looks and acts like a native app
+
+## TODO
+ * Look more into [MBP](https://github.com/h5bp/mobile-boilerplate/wiki) help.js and see if we should add it or if it conflicts with the little I have.
--- js/script.js
+++ js/script.js
@@ -3,30 +3,30 @@
   "use strict";
 
   // this is so the links stay with inside of our web app.
-  var iWebkit;
-  if (!iWebkit) {
-      iWebkit = window.onload = function () {
-          function fullscreen() {
-              var a = document.getElementsByTagName("a");
-              for (var i = 0; i < a.length; i++) {
-                  if (a[i].className.match("noeffect")) {} else {
-                      a[i].onclick = function () {
-                          window.location = this.getAttribute("href");
-                          return false
-                      }
-                  }
-              }
-          }
-          function hideURLbar() {
-              window.scrollTo(0, 0.9)
-          }
-          iWebkit.init = function () {
-              fullscreen();
-              hideURLbar()
-          };
-          iWebkit.init()
-      }
-  }
+  // var iWebkit;
+  // if (!iWebkit) {
+  //     iWebkit = window.onload = function () {
+  //         function fullscreen() {
+  //             var a = document.getElementsByTagName("a");
+  //             for (var i = 0; i < a.length; i++) {
+  //                 if (a[i].className.match("noeffect")) {} else {
+  //                     a[i].onclick = function () {
+  //                         window.location = this.getAttribute("href");
+  //                         return false
+  //                     }
+  //                 }
+  //             }
+  //         }
+  //         function hideURLbar() {
+  //             window.scrollTo(0, 0.9)
+  //         }
+  //         iWebkit.init = function () {
+  //             fullscreen();
+  //             hideURLbar()
+  //         };
+  //         iWebkit.init()
+  //     }
+  // }
 
 //
 // What we need to happen
@@ -50,3 +50,77 @@ if(document.URL.indexOf("index.html") >= 0){
 }
 
 }());
+
+
+
+//
+// Side Menu
+//
+$(document).ready(function(){
+  var pagebody = $("#page");
+  var themenu = $("nav");
+  var topbar  = $("header");
+  var content = $("#main");
+  var viewport = {
+      width  : $(window).width(),
+      height : $(window).height()
+  };
+  // retrieve variables as
+  // viewport.width / viewport.height
+
+  function openme() {
+    $(function () {
+        topbar.animate({
+           left: "255px"
+        }, { duration: 300, queue: false });
+        pagebody.animate({
+           left: "255px"
+        }, { duration: 300, queue: false });
+    });
+  }
+
+  function closeme() {
+    var closeme = $(function() {
+        topbar.animate({
+              left: "0px"
+        }, { duration: 180, queue: false });
+        pagebody.animate({
+              left: "0px"
+        }, { duration: 180, queue: false });
+    });
+  }
+
+  // checking whether to open or close nav menu
+  $("#menu-btn").on("click", function(e){
+    e.preventDefault();
+    var leftval = pagebody.css('left');
+
+    if(leftval == "0px") {
+      openme();
+    }
+    else {
+      closeme();
+    }
+  });
+
+  // loading page content for navigation
+  $("a.navlink").on("click", function(e){
+    e.preventDefault();
+    var linkurl     = $(this).attr("href");
+    var linkhtmlurl = linkurl.substring(1, linkurl.length);
+
+    var imgloader   = '<center style="margin-top: 30px;"><img src="img/preloader.gif" alt="loading..." /></center>';
+
+    closeme();
+
+    $(function() {
+      topbar.css("top", "0px");
+      window.scrollTo(0, 1);
+    });
+
+    content.html(imgloader);
+
+    setTimeout(function() { content.load(linkhtmlurl, function() { /* no callback */ }) }, 1200);
+  });
+});
+
